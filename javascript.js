d8fe40f8d3fa34c84dd4a68dd1283077c6715d5d
Michael Tuepker <193541888+Bobsled6@users.noreply.github.com>
2025-05-09 18:50:56 -0400

    added for loop to create divs

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let container = document.querySelector(".container");
+
+for(let i = 0; i < 16; i++){
+    let etchDiv = document.createElement("div");
+    etchDiv.className = "sketch";
+    container.appendChild(etchDiv);
+}
+
