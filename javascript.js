1d08a00c5092f8a68bb832078d8a508c40f0f108
Michael Tuepker <193541888+Bobsled6@users.noreply.github.com>
2025-05-11 19:18:48 -0400

    added for loop to create 16 divs and function to calc user prompt grid(still need to combine them)

--- javascript.js
+++ javascript.js
@@ -1,8 +1,16 @@
 let container = document.querySelector(".container");
 
+
 for(let i = 0; i < 16; i++){
     let etchDiv = document.createElement("div");
     etchDiv.className = "sketch";
+    etchDiv.style.width = "150px";
+    etchDiv.style.height = "150px";
     container.appendChild(etchDiv);
 }
 
+function dimCalc(userPrompt){
+    userPrompt = prompt();
+    let dimDiv = Math.sqrt(360000 / userPrompt);
+    return(dimDiv);
+    }
