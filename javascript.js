6dd197c13c9d6449c2748df22e29b6ee6b268cf7
Michael Tuepker <193541888+Bobsled6@users.noreply.github.com>
2025-05-12 18:15:02 -0400

    for loop generates randomly colored div,still need to randomize on each mouseover

--- javascript.js
+++ javascript.js
@@ -1,13 +1,21 @@
 let container = document.querySelector(".container");
-let rbgStringArray = new Array();
+const rgbStringArray = new Array();
+
+for(let i = 0; i < 3; i++){
+    let colorValue = (Math.random() * 256);
+    colorValue = Math.round(colorValue);
+    rgbStringArray.push(colorValue);
+}
+
+
 
 for(let i = 0; i < 16; i++){
-    let etchDiv = document.createElement("div");
+    let etchDiv = document.createElement("div");  
     etchDiv.className = "sketch";
     etchDiv.style.width = "150px";
     etchDiv.style.height = "150px";
     etchDiv.addEventListener("mouseenter", () => {
-        etchDiv.style.backgroundColor = "blue";
+        etchDiv.style.backgroundColor = "rgb("+ rgbStringArray[0] +", " + rgbStringArray[1] + ", " + rgbStringArray[2] + ")";
     })
     container.appendChild(etchDiv);
 }
@@ -17,12 +25,6 @@ function dimCalc(userPrompt){
         let dimDiv = Math.sqrt(360000 / userPrompt);
         return(dimDiv);
     }   else{alert("ERROR: Please enter a whole number between 1 and 100")}
-}
-
-for(let i = 0; i < 3; i++){
-    let colorValue = (Math.random() * 256);
-    colorValue = Math.round(colorValue);
-    rbgString = rbgStringArray.push(colorValue);
 }
 
 
