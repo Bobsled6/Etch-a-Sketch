31d0c819e0b8f821bc24e03f7965be46cc1d92a9
Michael Tuepker <193541888+Bobsled6@users.noreply.github.com>
2025-05-13 10:55:47 -0400

    fixed dimCalc function,was solving for the wrong variable

--- javascript.js
+++ javascript.js
@@ -1,28 +1,29 @@
 let container = document.querySelector(".container");
+let divButton = document.createElement("button")
 
 
-
-
-for(let i = 0; i < 16; i++){
+for(let i = 0; Math.sqrt(i) < 16; i++){
     let etchDiv = document.createElement("div");  
-    etchDiv.className = "sketch";
-    etchDiv.style.width = "150px";
-    etchDiv.style.height = "150px";
-    etchDiv.addEventListener("mouseover", () => {
-        let rgbStringArray = new Array();
-        for(let i = 0; i < 3; i++){
-            let colorValue = (Math.random() * 256);
-            colorValue = Math.round(colorValue);
-            rgbStringArray.push(colorValue);
-        }   
-    etchDiv.style.backgroundColor = "rgb("+ rgbStringArray[0] +", " + rgbStringArray[1] + ", " + rgbStringArray[2] + ")";
-    })
+        etchDiv.className = "sketch";
+        etchDiv.style.width = "37.5px";
+        etchDiv.style.height = "37.5px";
+        etchDiv.addEventListener("mouseenter", () => {
+            let rgbStringArray = new Array();
+            for(let i = 0; i < 3; i++){
+                let colorValue = (Math.random() * 256);
+                colorValue = Math.round(colorValue);
+                rgbStringArray.push(colorValue);
+        }   etchDiv.style.backgroundColor = "rgb("+ rgbStringArray[0] +
+                                             ", " + rgbStringArray[1] +
+                                             ", " + rgbStringArray[2] + ")";})                              
     container.appendChild(etchDiv);
 }
 
 function dimCalc(userPrompt){
     if(userPrompt <= 100 && userPrompt > 0 && Number.isInteger(userPrompt)){
-        let dimDiv = Math.sqrt(360000 / userPrompt);
+        let dimDiv = (1 / userPrompt) * 600;
         return(dimDiv);
-    }   else{alert("ERROR: Please enter a whole number between 1 and 100")}
+    }   else{alert("ERROR: Please enter a whole number between 1 and 100.")}
 }
+
+console.log(dimCalc(16));
