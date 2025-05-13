5dc4e8a5527b132dd03ce77be37fe125e7eb75d4
Michael Tuepker <193541888+Bobsled6@users.noreply.github.com>
2025-05-13 12:19:52 -0400

    added button, event listener(not finished)

--- javascript.js
+++ javascript.js
@@ -1,12 +1,21 @@
 let container = document.querySelector(".container");
-let divButton = document.createElement("button")
+let divButton = document.createElement("button");
+let bigContainer = document.querySelector(".bigContainer");
+let userChoice = 16;
+createGrid();
 
+function dimCalc(userPrompt){
+    if(userPrompt <= 100 && userPrompt > 0 && Number.isInteger(userPrompt)){
+        let dimDiv = (1 / userPrompt) * 600;
+        return(dimDiv);
+    }   else{alert("ERROR: Please enter a whole number between 1 and 100.")}
+}
 
-for(let i = 0; Math.sqrt(i) < 16; i++){
+function createGrid (){for(let i = 0; Math.sqrt(i) < userChoice; i++){
     let etchDiv = document.createElement("div");  
         etchDiv.className = "sketch";
-        etchDiv.style.width = "37.5px";
-        etchDiv.style.height = "37.5px";
+        etchDiv.style.width = dimCalc(userChoice) + "px";
+        etchDiv.style.height = dimCalc(userChoice) + "px";
         etchDiv.addEventListener("mouseenter", () => {
             let rgbStringArray = new Array();
             for(let i = 0; i < 3; i++){
@@ -17,13 +26,17 @@ for(let i = 0; Math.sqrt(i) < 16; i++){
                                              ", " + rgbStringArray[1] +
                                              ", " + rgbStringArray[2] + ")";})                              
     container.appendChild(etchDiv);
-}
-
-function dimCalc(userPrompt){
-    if(userPrompt <= 100 && userPrompt > 0 && Number.isInteger(userPrompt)){
-        let dimDiv = (1 / userPrompt) * 600;
-        return(dimDiv);
-    }   else{alert("ERROR: Please enter a whole number between 1 and 100.")}
-}
+}}
 
 console.log(dimCalc(16));
+
+divButton.textContent = "New Grid";
+
+divButton.addEventListener("click", () => {
+    promptAnswer = prompt("Please enter the amount of squares per side of your new grid. (max 100)");
+    userChoice = promptAnswer * 1;
+    createGrid();
+});
+
+bigContainer.appendChild(divButton);
+bigContainer.appendChild(container);
